4c7cf4c84047819cc552771e2b7ee19a0081d341
Tom Bee <tburzynski2@yahoo.com>
2024-05-09 20:30:18 -0500

    dynamic port

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-const express = require('express')
-const requestLogger = require('./middleware/requestLogger')
-const blockPrivateRoutes = require('./middleware/blockPrivateRoutes')
-const routes = require('./routes')
+const express = require("express");
+const requestLogger = require("./middleware/requestLogger");
+const blockPrivateRoutes = require("./middleware/blockPrivateRoutes");
+const routes = require("./routes");
 
-const app = express()
-const PORT = 3001
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 // global middleware
 
-app.use( requestLogger )
-app.use( blockPrivateRoutes )
+app.use(requestLogger);
+app.use(blockPrivateRoutes);
 
-app.use( express.static('public') )
-app.use( express.json() )
+app.use(express.static("public"));
+app.use(express.json());
 
-app.use( routes )
+app.use(routes);
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
-})
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
